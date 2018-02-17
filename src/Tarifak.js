e68773db34be88f19bb09d027effa08a3fca8a40
Attila Kiss <atkiss@gmail.com>
2018-02-17 14:40:42 +0000

    Afa modositas

--- src/Tarifak.js
+++ src/Tarifak.js
@@ -1,15 +1,15 @@
 export const korlatlanTarifa = [
     {tipus: 'SMS', nettoegysegar: 20, afakulcs: 27},
     {tipus: 'HAVIDIJ', nettoegysegar: 6300, afakulcs: 27},
-    {tipus: 'MOBIL_INTERNET_100M', nettoegysegar: 450, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_500M', nettoegysegar: 1116, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_1G', nettoegysegar: 1464, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_2G', nettoegysegar: 1683, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_4G', nettoegysegar: 2400, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_7G', nettoegysegar: 3423, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_13G', nettoegysegar: 4438, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_24G', nettoegysegar: 6400, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_50G', nettoegysegar: 8000, afakulcs: 18}
+    {tipus: 'MOBIL_INTERNET_100M', nettoegysegar: 450, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_500M', nettoegysegar: 1116, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_1G', nettoegysegar: 1464, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_2G', nettoegysegar: 1683, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_4G', nettoegysegar: 2400, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_7G', nettoegysegar: 3423, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_13G', nettoegysegar: 4438, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_24G', nettoegysegar: 6400, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_50G', nettoegysegar: 8000, afakulcs: 5}
 ]
 
 export const simaTarifa = [
@@ -20,15 +20,15 @@ export const simaTarifa = [
     {tipus: 'BELFOLDI_VEZETEKES', nettoegysegar: 12, afakulcs: 27},
     {tipus: 'SMS', nettoegysegar: 20, afakulcs: 27},
     {tipus: 'HAVIDIJ', nettoegysegar: 2200, afakulcs: 27},
-    {tipus: 'MOBIL_INTERNET_100M', nettoegysegar: 450, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_500M', nettoegysegar: 1116, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_1G', nettoegysegar: 1464, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_2G', nettoegysegar: 1683, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_4G', nettoegysegar: 2400, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_7G', nettoegysegar: 3423, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_13G', nettoegysegar: 4438, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_24G', nettoegysegar: 6400, afakulcs: 18},
-    {tipus: 'MOBIL_INTERNET_50G', nettoegysegar: 8000, afakulcs: 18},
+    {tipus: 'MOBIL_INTERNET_100M', nettoegysegar: 450, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_500M', nettoegysegar: 1116, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_1G', nettoegysegar: 1464, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_2G', nettoegysegar: 1683, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_4G', nettoegysegar: 2400, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_7G', nettoegysegar: 3423, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_13G', nettoegysegar: 4438, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_24G', nettoegysegar: 6400, afakulcs: 5},
+    {tipus: 'MOBIL_INTERNET_50G', nettoegysegar: 8000, afakulcs: 5},
     {tipus: 'KEDVEZMENY', nettoegysegar: 12, mennyiseg: 150, afakulcs: 27},
     {tipus: 'EGYCSOPORT', nettoegysegar: 400, afakulcs: 27}
 ]
